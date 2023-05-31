d2eb010748d4f212144ae2cce74b62c646681d2f
nicolapieri <nicolapieri061099@gmail.com>
2023-05-31 18:10:20 +0200

    DJI Beta included

--- stocksbeta.py
+++ stocksbeta.py
@@ -2,8 +2,11 @@ import numpy as np
 import yvariables as yv
 
 # computing the Beta
+Stocks_Betas = []
 for symbol in yv.composition:
     globals()[symbol + '_Beta'] = np.polyfit(yv.Returns['DJI_Returns'],
                                              yv.Returns[f'{symbol}_Returns'], 1)[0]
+    Stocks_Betas.append(globals()[symbol + '_Beta'])
 
-print(MMM_Beta, )
+DJI_Beta = sum(Stocks_Betas) / len(Stocks_Betas)
+print(DJI_Beta)
