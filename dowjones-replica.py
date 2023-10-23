2cf34337b996cb457d712e60a1fe49e86f226929
nicolapieri <nicolapieri061099@gmail.com>
2023-10-23 16:40:45 +0200

    ci: fix testing procedures

--- dowjones-replica.py
+++ dowjones-replica.py
@@ -1,6 +1,12 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import time
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 # NNLS
 from scipy.optimize import nnls
 # PCRR (necessary for partial corr)
@@ -248,3 +254,135 @@ def choose_predictors():
     else:
         print("Invalid input. Please try again.")
         return choose_predictors()
+
+
+# Dow Jones compositions over years
+compat_2003_01_27 = ["MMM", "KODK", "JNJ", "AA", "XOM", "JPM", "MO", "GE", "MCD", "AXP",
+                     "GM", "MRK", "T", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "HON",
+                     "SBAC", "C", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "IP", "DIS"]
+
+compat_2004_04_08 = ["MMM", "PFE", "JNJ", "AA", "XOM", "JPM", "MO", "GE", "MCD", "AXP",
+                     "GM", "MRK", "AIG", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "HON",
+                     "SBAC", "C", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2005_11_21 = ["MMM", "PFE", "JNJ", "AA", "XOM", "JPM", "MO", "GE", "MCD", "AXP",
+                     "GM", "MRK", "AIG", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "HON",
+                     "T", "C", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2008_02_19 = ["MMM", "PFE", "JNJ", "AA", "XOM", "JPM", "BAC", "GE", "MCD", "AXP",
+                     "GM", "MRK", "AIG", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "T", "C", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2008_09_22 = ["MMM", "PFE", "JNJ", "AA", "XOM", "JPM", "BAC", "GE", "MCD", "AXP",
+                     "GM", "MRK", "KHC", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "T", "C", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2009_06_08 = ["MMM", "PFE", "JNJ", "AA", "XOM", "JPM", "BAC", "GE", "MCD", "AXP",
+                     "TRV", "MRK", "KHC", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "T", "CSCO", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2012_09_24 = ["MMM", "PFE", "JNJ", "AA", "XOM", "JPM", "BAC", "GE", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "HPQ", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "T", "CSCO", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2013_09_23 = ["MMM", "PFE", "JNJ", "GS", "XOM", "JPM", "NKE", "GE", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "V", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "T", "CSCO", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2015_03_19 = ["MMM", "PFE", "JNJ", "GS", "XOM", "JPM", "NKE", "GE", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "V", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "AAPL", "CSCO", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2018_06_26 = ["MMM", "PFE", "JNJ", "GS", "XOM", "JPM", "NKE", "WBA", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "V", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "AAPL", "CSCO", "INTC", "UTX", "KO", "IBM", "WMT", "DD", "VZ", "DIS"]
+
+compat_2019_04_02 = ["MMM", "PFE", "JNJ", "GS", "XOM", "JPM", "NKE", "WBA", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "V", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "AAPL", "CSCO", "INTC", "UTX", "KO", "IBM", "WMT", "DOW", "VZ", "DIS"]
+
+compat_2020_04_06 = ["MMM", "PFE", "JNJ", "GS", "XOM", "JPM", "NKE", "WBA", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "V", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "AAPL", "CSCO", "INTC", "RTX", "KO", "IBM", "WMT", "DOW", "VZ", "DIS"]
+
+compat_2020_08_31 = ["MMM", "AMGN", "JNJ", "GS", "HON", "JPM", "NKE", "WBA", "MCD", "AXP",
+                     "TRV", "MRK", "UNH", "V", "MSFT", "BA", "HD", "PG", "CAT", "CVX",
+                     "AAPL", "CSCO", "INTC", "CRM", "KO", "IBM", "WMT", "DOW", "VZ", "DIS"]
+
+# applying optimization methods
+test_opt(compat_2003_01_27, '2003-01-27', '2003-06-01', '2004-04-08')
+test_opt(compat_2004_04_08, '2004-04-08', '2004-04-09', '2005-11-21')
+test_opt(compat_2005_11_21, '2005-11-21', '2005-11-22', '2008-02-19')
+test_opt(compat_2008_02_19, '2008-02-19', '2008-02-20', '2008-09-22')
+test_opt(compat_2008_09_22, '2008-09-22', '2008-09-23', '2009-06-08')
+test_opt(compat_2009_06_08, '2009-06-08', '2009-06-09', '2012-09-24')
+test_opt(compat_2012_09_24, '2012-09-24', '2012-09-25', '2013-09-23')
+test_opt(compat_2013_09_23, '2013-09-23', '2013-09-24', '2015-03-19')
+test_opt(compat_2015_03_19, '2015-03-19', '2015-03-20', '2018-06-26')
+test_opt(compat_2018_06_26, '2018-06-26', '2018-06-27', '2019-04-02')
+test_opt(compat_2019_04_02, '2019-04-02', '2019-04-03', '2020-04-06')
+test_opt(compat_2020_04_06, '2020-04-06', '2020-04-07', '2020-08-31')
+test_opt(compat_2020_08_31, '2020-08-31', '2020-09-01', '2023-05-31')
+
+# aggregating results
+Fin = pd.concat(
+    [Opt_2003_01_27, Opt_2004_04_08, Opt_2005_11_21, Opt_2008_02_19, Opt_2008_09_22, Opt_2009_06_08, Opt_2012_09_24,
+     Opt_2013_09_23, Opt_2015_03_19, Opt_2018_06_26, Opt_2019_04_02, Opt_2020_04_06, Opt_2020_08_31])
+
+TE = pd.DataFrame()
+TE['NNLS'] = round((Fin['NNLS'].pct_change().dropna() - Fin['^DJI'].pct_change().dropna()) * 10000, 4)
+TE['PCRR'] = round((Fin['PCRR'].pct_change().dropna() - Fin['^DJI'].pct_change().dropna()) * 10000, 4)
+TE['DTW'] = round((Fin['DTW'].pct_change().dropna() - Fin['^DJI'].pct_change().dropna()) * 10000, 4)
+TE['NNMF'] = round((Fin['NNMF'].pct_change().dropna() - Fin['^DJI'].pct_change().dropna()) * 10000, 4)
+TE['PSO'] = round((Fin['PSO'].pct_change().dropna() - Fin['^DJI'].pct_change().dropna()) * 10000, 4)
+TE[(TE.index >= pd.to_datetime('2023-05-01')) & (TE.index <= pd.to_datetime('2023-05-31'))].plot()
+TE['Mean'] = TE.mean(axis=1)
+TE['Replica'] = (abs(TE['Mean']) < 5).astype(bool)
+
+plt.title(f"Optimizations Tracking Errors (last month only)")
+plt.ylabel('Basis Points (bps)')
+plt.hlines(y=5, xmin="2023-05-01", xmax="2023-05-31", colors='indigo', linestyle='dashed')
+plt.hlines(y=-5, xmin="2023-05-01", xmax="2023-05-31", colors='indigo', linestyle='dashed')
+plt.show()
+print(TE)
+
+# train-test split
+train_TE = TE[(TE.index >= pd.to_datetime('2003-06-01')) & (TE.index <= pd.to_datetime('2019-05-31'))]
+test_TE = TE[(TE.index >= pd.to_datetime('2019-06-01')) & (TE.index <= pd.to_datetime('2023-05-31'))]
+epochs = 20
+
+# choosing predictors
+print("-" * 100)
+predictors = choose_predictors()
+
+# ML modelling
+ml_model = RandomForestClassifier(n_estimators=100, min_samples_split=50, random_state=1)
+ml_model.fit(train_TE[predictors], train_TE['Replica'])
+
+# DL modelling
+dl_model = Sequential()
+dl_model.add(Dense(4, input_shape=(2,), activation='relu'))
+dl_model.add(Dense(2, activation='relu'))
+dl_model.add(Dense(1, activation='sigmoid'))
+dl_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+t1 = time.time()
+dl_model.fit(train_TE[predictors], train_TE['Replica'], epochs=epochs, batch_size=10)
+t2 = time.time()
+print("-" * 100)
+print('Elapsed seconds per epoch: {:.5f}'.format((t2 - t1) / epochs))
+dl_model.summary()
+
+# presenting results
+replica_preds = pd.DataFrame()
+replica_preds['Ground_Truth'] = test_TE[['Replica']]
+replica_preds['ML_preds'] = pd.Series(ml_model.predict(test_TE[predictors]), index=test_TE.index)
+replica_preds['DL_preds'] = (dl_model.predict(test_TE[predictors]) > 0.5).astype(bool)
+
+print("-" * 100)
+print(f'Used the pair of optimizers {predictors} as predictors.')
+print("-" * 100)
+print(replica_preds)
+print("-" * 100)
+print('ML accuracy score:', precision_score(replica_preds['Ground_Truth'], replica_preds['ML_preds']))
+print("-" * 100)
+print('DL accuracy score:', precision_score(replica_preds['Ground_Truth'], replica_preds['DL_preds']))
